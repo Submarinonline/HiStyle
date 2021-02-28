60be3a962ddb6f5aff8daef0fca693d87fddf924
hakunagi <hakunagi.rei@gmail.com>
2021-03-01 00:27:39 +0900

    いろいろやった

--- js/histyle-main.js
+++ js/histyle-main.js
@@ -1,5 +1,15 @@
-$(function () {
-  $('.hs-switch-theme').click(function () { 
-    $('html').toggleClass('dark');
-  });
-});
+const hs = {
+  switchTheme: (() => $(`html`).toggleClass(`hs-dark`)),
+  switchMenu: (() => {
+    $(`.hs-menu`).toggleClass(`hs-menu-open`)
+    if ($(`.hs-menu`).hasClass(`hs-menu-open`)) {
+      $(`.hs-fixednav`).css('top', $(`.hs-menu`).height())
+      $(`.hs-menu`).css('top', '0')
+    } else {
+      $(`.hs-fixednav`).css('top', '0')
+      $(`.hs-menu`).css('top', -$(`.hs-menu`).height())
+    }
+  }),
+};
+
+$(`.hs-menu`).css('top', -$(`.hs-menu`).height());
