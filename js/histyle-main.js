ee068c8fe7382141fb2a4eae633ba5dc7b40bdeb
hakunagi <hakunagi.rei@gmail.com>
2021-03-02 01:02:41 +0900

    scssを分割 なんか修正

--- js/histyle-main.js
+++ js/histyle-main.js
@@ -1,15 +1,24 @@
+let userAgent = window.navigator.userAgent.toLowerCase();
+
 const hs = {
   switchTheme: (() => $(`html`).toggleClass(`hs-dark`)),
   switchMenu: (() => {
-    $(`.hs-menu`).toggleClass(`hs-menu-open`)
+    $(`.hs-menu`).toggleClass(`hs-menu-open`);
     if ($(`.hs-menu`).hasClass(`hs-menu-open`)) {
-      $(`.hs-fixednav`).css('top', $(`.hs-menu`).height())
-      $(`.hs-menu`).css('top', '0')
+      $(`.hs-fixednav`).css('top', $(`.hs-menu`).height());
+      $(`.hs-menu`).css('top', '0');
     } else {
-      $(`.hs-fixednav`).css('top', '0')
-      $(`.hs-menu`).css('top', -$(`.hs-menu`).height())
-    }
+      $(`.hs-fixednav`).css('top', '0');
+      $(`.hs-menu`).css('top', -$(`.hs-menu`).height());
+    };
   }),
 };
 
-$(`.hs-menu`).css('top', -$(`.hs-menu`).height());
+(() => {
+  $(`.hs-menu`).css('top', -$(`.hs-menu`).height());
+
+  if (userAgent.indexOf('firefox') != -1) {
+    $(`.hs-fixednav`).toggleClass(`hs-ua-firefox`);
+    $(`.hs-menu`).toggleClass(`hs-ua-firefox`);
+  }
+})();
